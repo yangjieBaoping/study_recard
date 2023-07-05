131a73fbd7c56208e64cd90b7c01465a4aebbcbe
yangjieBaoping <yangjie199902@outlook.com>
2023-07-05 17:29:46 +0800

    fix

--- shopping/src/views/HomeView.vue
+++ shopping/src/views/HomeView.vue
@@ -1,27 +1,76 @@
 <template>
-  <div id="home_page">
-    <div
-      style="
-        height: 30px;
-        line-height: 30px;
-        background-color: rgb(0, 119, 255);
-      "
-    >
-      离结束还有<span
-        style="color: red; font-size: 20px; font-weight: bolder"
-        >{{}}</span
-      >天
+  <div>
+    <div id="home_head">
+      <div v-for="item in labelList">
+        <div class="label_item" style="padding: 10px 20px; position: relative">
+          {{ item.name }}
+          <div class="label_item_toosip">
+            <div
+              style="text-align: center; border-bottom: 1px solid #999"
+              v-for="Item in item.childs"
+            >
+              {{ Item.name }}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-// 时间相关
+import { reactive } from "vue";
+
+const labelList = reactive([
+  {
+    name: "首页",
+    childs: [],
+  },
+  {
+    name: "生活",
+    childs: [
+      {
+        name: "米食",
+      },
+      {
+        name: "蔬果",
+      },
+      {
+        name: "衣物",
+      },
+    ],
+  },
+  {
+    name: "电子产品",
+    childs: [
+      {
+        name: "手机数码",
+      },
+      {
+        name: "电子手表",
+      },
+    ],
+  },
+]);
 </script>
 
 <style scoped lang="scss">
-#home_page {
-  margin: 0px;
-  padding: 0px;
+#home_head {
+  height: 50px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  box-shadow: 0px 0px 5px 0px #f6f6f6;
+}
+
+.label_item .label_item_toosip {
+  position: absolute;
+  top: 51px;
+  left: 0px;
+  visibility: hidden;
+}
+.label_item:hover .label_item_toosip {
+  visibility: visible;
+  padding: 10px 20px;
 }
 </style>
--- 示例/figure_motion/src/App.vue
+++ 示例/figure_motion/src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <!-- 主界面 -->
   <div id="first_card">
-    <div style="width: 1000px; height: 100%"></div>
-    <!-- <div style="width: 1000px; height: 100%; background-color: yellow"></div>
-    <div style="width: 1000px; height: 100%; background-color: blue"></div> -->
     <!-- 人物 -->
     <getPeople />
   </div>
@@ -28,7 +25,6 @@ onMounted(() => {
 #first_card {
   width: 100vw;
   height: 100vh;
-  display: flex;
 }
 /* 清除滚动条 */
 #first_card::-webkit-scrollbar {
