8ddbabb3a7854e7562ac3e185669fc34f13e9387
yangjieBaoping <yangjie199902@outlook.com>
2023-08-04 15:40:04 +0800

    地图构建

--- Example/mapAndWeather/src/views/HomeView.vue
+++ Example/mapAndWeather/src/views/HomeView.vue
@@ -1,47 +1,35 @@
 <template>
-  <!-- <div id="home_bdoy">
-    <div id="home_head"></div>
-    <div id="home_body"></div>
-  </div> -->
+  <!-- 地区 -->
   <div id="container"></div>
+  <div>当前地区</div>
+  <div>{{ currentArea.value }}</div>
+  <!-- 天气 -->
+  <div>近几日天气</div>
 </template>
 
-<script type="text/javascript">
-window._AMapSecurityConfig = {
-  securityJsCode: 'b7c54ae310da8d79021001ff2a1eab67',
-}
-</script>
-<!-- <script src="https://webapi.amap.com/loader.js"></script> -->
-<!-- <script type="text/javascript">
-AMapLoader.load({
-  key: 'a04f0e0ac72267d014ef75f4f3a6ab39', // 申请好的Web端开发者Key，首次调用 load 时必填
-  version: '2.0', // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
-})
-  .then(AMap => {
-    const map = new AMap.Map('container')
-  })
-  .catch(e => {
-    console.error(e) //加载错误提示
-  })
-</script> -->
 <script setup>
-// 判断当前环境
-const getEnvironment = () => {
-  console.log(navigator.userAgent)
-}
-getEnvironment()
+import { reactive } from 'vue'
+import AMapLoader from '@amap/amap-jsapi-loader'
+
+const currentArea = reactive({
+  value: '',
+})
+// AMapLoader.load({
+//   key: 'a04f0e0ac72267d014ef75f4f3a6ab39', // 申请好的Web端开发者Key，首次调用 load 时必填
+//   version: '2.0', // 指定要加载的 JS API 的版本，缺省时默认为 1.4.15
+//   plugins: [], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+// })
+//   .then(AMap => {
+//     map = new AMap.Map('container')
+//   })
+//   .catch(e => {
+//     console.log(e)
+//   })
 </script>
 
 <style scoped lang="scss">
-// #home {
-//   height: 50px;
-//   background-color: rgb(0, 204, 255);
-// }
-// #home_body {
-//   height: calc(100vh - 50px);
-//   overflow-y: scroll;
-// }
-// #home_body::-webkit-scrollbar {
-//   display: none;
-// }
+#container {
+  width: calc(100vw);
+  height: 300px;
+}
 </style>
