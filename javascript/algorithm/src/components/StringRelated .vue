7a215cbd414af76b9fe11eaabb6f771f986d9935
yangjieBaoping <yangjie199902@outlook.com>
2023-08-25 17:19:20 +0800

    fix

--- javascript/algorithm/src/components/StringRelated .vue
+++ javascript/algorithm/src/components/StringRelated .vue
@@ -12,6 +12,16 @@
     <input type="text" ref="hump_str" class="input_text" @change="getHump" />
     <div>结果</div>
     <input type="text" ref="hump_str_result" class="input_text" />
+    <div>清空字符串两端空格</div>
+    <input type="text" ref="space_str" class="input_text" @change="getSpace" />
+    <div>将数字转化为RMB形式</div>
+    <input type="text" ref="rmb_str" class="input_text" @change="getRMB" />
+    <div>结果</div>
+    <input type="text" ref="rmb_str_result" class="input_text" />
+    <div>删除相邻相同的字符</div>
+    <input type="text" ref="adjacent_str" class="input_text" @change="getAdjacent" />
+    <div>结果</div>
+    <input type="text" ref="adjacent_str_text" class="input_text" />
   </div>
 </template>
 
@@ -23,6 +33,11 @@ const reversal_str_result = ref('')
 const moslems_str = ref('')
 const hump_str = ref('')
 const hump_str_result = ref('')
+const space_str = ref('')
+const rmb_str = ref('')
+const rmb_str_result = ref('')
+const adjacent_str = ref('')
+const adjacent_str_text = ref('')
 
 const moslems = reactive({ value: false })
 
@@ -53,6 +68,45 @@ const getHump = () => {
     arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].substring(1)
   }
   hump_str_result.value.value = arr.join('')
+}
+
+// 清除字符串两端空白
+const getSpace = () => {
+  let str = space_str.value.value
+  return str.replace(/(^\s*)|(\s*$)/g, '')
+}
+
+// 将数字转化为RMB格式
+const getRMB = () => {
+  let num = rmb_str.value.value
+  if (num.length === 0) return
+  if (isNaN(Number(num))) return
+  let arr = num.split('').reverse()
+  if (arr.length % 3 === 0) return
+  let res = []
+  for (let i = 0; i < arr.length; i++) {
+    res.push(arr[i])
+    if ((i + 1) % 3 === 0) {
+      res.push(',')
+    }
+  }
+  rmb_str_result.value.value = res.reverse().join('')
+}
+
+// 删除相邻相同的字符
+const getAdjacent = () => {
+  let res = [],
+    newStr,
+    str
+  str = adjacent_str.value.value
+  if (str.length === 0) return
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].charAt(0) !== newStr) {
+      res.push(str[i].charAt(0))
+      newStr = str[i].charAt(0)
+    }
+  }
+  adjacent_str_text.value.value = res.join('')
 }
 </script>
 
